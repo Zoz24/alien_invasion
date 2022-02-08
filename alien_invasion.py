a1268f9dc9711f9fc911f46f61b51fc54fdb684e
Zizhan <zizhanzhou24@gmail.com>
2022-02-08 09:37:22 -0500

    added function for ship movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,4 @@
+from doctest import FAIL_FAST
 import sys
 import pygame
 from settings import Settings
@@ -10,14 +11,38 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        # self.bg_color = (230, 230, 230)
         self.ship = Ship(self)
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self.check_events()
+            self.ship.update()
+            self.update_screen()
 
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self.check_keydown_events(event)     
+            elif event.type == pygame.KEYUP:
+                self.check_keyup_events(event)
+
+
+    def check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+    
+    def check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def update_screen(self):
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
             pygame.display.flip()
