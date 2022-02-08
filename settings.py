33e6c9542d36de593bc4490627bc99039f772cb8
Zizhan <zizhanzhou24@gmail.com>
2022-02-08 17:34:51 -0500

    added bullet functionality

--- settings.py
+++ settings.py
@@ -6,6 +6,9 @@ class Settings:
         self.ship_speed = 1.5
         self.bullet_speed = 1.0
         self.bullet_height = 15
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
